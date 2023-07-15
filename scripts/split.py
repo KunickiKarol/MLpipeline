6ec60925546bde2b23f5b09ab0477a62cc7df09a
kaka <kunicki.karol1@gmail.com>
2023-07-15 22:46:14 +0200

    start using mypy part2

--- scripts/split.py
+++ scripts/split.py
@@ -6,6 +6,40 @@ import dvc.api
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+import json
+import os
+
+import dvc.api
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from typing import Tuple
+
+
+def split_json_file(start_file: str, target_file_train: str, target_file_test: str, class_column_name: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    df = pd.read_json(start_file, lines=True)
+
+    train, test = train_test_split(df, test_size=0.2, stratify=df[class_column_name])
+
+    return train, test
+
+
+def main() -> None:
+    params = dvc.api.params_show()
+
+    start_file = params["preprocessing"]["target_file"]
+    target_file_train = params["split"]["target_file_train"]
+    target_file_test = params["split"]["target_file_test"]
+    class_column_name = params["class_column_name"]
+
+    train, test = split_json_file(
+        start_file, target_file_train, target_file_test, class_column_name
+    )
+    train.to_json(target_file_train, orient="records", lines=True)
+    test.to_json(target_file_test, orient="records", lines=True)
+
+
+if __name__ == "__main__":
+    main()
 
 def split_json_file(
     start_file: str,
